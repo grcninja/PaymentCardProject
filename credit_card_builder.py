c3cc72012f4eddd5da04334231da702520888c00
grcninja <grcninja@users.noreply.github.com>
2015-05-06 12:08:28 -0500

    Visa Only in this version
    This code will work if you enter a 4 or VISA - the next release will contain the other vendor cards

--- credit_card_builder.py
+++ credit_card_builder.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-#This program is designed to guess possible credit card numbers give partial information
+#This program is designed to identify valid credit card numbers given partial information
 #coder syntax note:  I prefer to use the lower case g at the beginning of a global variable name to easily distinguish it for those nights when I'm up entirely too late and shouldn't be coding
 
 from cards import payment_card
 import datetime
 import math
+import os
 import string
 import sys
 
+
 dt = datetime.date.today()
 PaymentCard = payment_card.PaymentCard("0", "UNK", "000000", "000000000000", dt, "00000000", "John", "Q", "Doe", "")
-gValidCardNumbers = []
 gAllowedCardTypes = PaymentCard.tAllowedCardTypes
 gAllowedCardInput = ("0","1","2","3","4","5","6","7","8","9","?") #this is a tuple, which cannot be modified unless deliberately converted to a list, modified then converted back to a tuple
 
-def generateNumberList(count_of_unknown_numbers):
+def generateReplNumsList(count_of_unknown_numbers):
 	"""
 	The mathematical formula to calculate the number of permutations is n^r where n is the number of options
 	in this case we have (0-9) giving us 10 options.  And r = the permutations, the count of unknown numbers
@@ -23,125 +24,76 @@ def generateNumberList(count_of_unknown_numbers):
 	count = 10**unkNums  #n^r
 	my_list = list() 
 	i = 0	
-	#generate the digits that will be needed, and store them in the list
 	for i in range (count):
 		n = str(i)
-		if len(n) < unkNums:#we need to prepend 0's if the length of the string of numbers is less than the count of unknown numbers
-			padding = unkNums - len(n) #figure out how many 0's we need
-			n = n.zfill(padding)
+		if len(n) < unkNums:
+			padding = int(unkNums - len(n)) 
+			n = n.zfill(padding+1)
 			my_list.append(n)
 		else:
 			my_list.append(n)
 	return(my_list)
 
-def validateCard(fCardNumber):
-	#The following code was written by JC-SoCal and can be found here:
-	#    https://github.com/JC-SoCal/CC_Playground/blob/master/ccluhnbrute.py
-	r = [int(ch) for ch in str(fCardNumber)][::-1]
+def validateCard(cardstring):
+	#perform a check using the Luhn algorithm
+	#The following was written by https://github.com/jCh0x61/Luhn-Algorithm/blob/master/LuhnChecker.py
+	y = cardstring
+	c = len(y) - 1
+	parity = len(y) & 1
+	sum=0
 
+	while True:
+		n=int(y[c])
+		if c & 1 == parity:
+			n*=2
+		if n > 9:
+			n = n - 9
+		sum=sum+n
+		c += -1
+		if c < 0:
+			break
+	return ((sum % 10) == 0)
+
+def buildList(thelist, thecard):
+	mylist = thelist
+	mystring = thecard
+	count = thecard.count("?")
 	
+	newlist = list()
+	newstring = ""
+	if len(mylist[1]) == count:
+		for i in range (len(mylist)): 
+			newstring = mystring
+			replchars = mylist[i]
+			for x in range (count): 
+				y=0
+				y2 = len(replchars)
+				for z in range (len(newstring)):
+					if newstring[z] =="?":
+						newstring = newstring.replace(newstring[z],replchars[y],1)
+						y+=1
+			newlist.append(newstring)				
+	else:
+		print("There is a problem with the replacement chars\n they are too short/long. \nThey have "+str(len(mylist[1]))+" characters and we need "+str(count))
+	
+	return newlist
+			
 def generateCardNumbers(pCardNumber, brand):
 	partial_card = pCardNumber
-	valid_numbers =()
-	master_list = list() #not setting a max length leaves room for the list to grow, so we'll set it later
+	valid_numbers = list()
+	replnum_list = list() #not setting a max length leaves room for the list to grow, so we'll set it later
 	lbrand = brand
 	#This function expects the following values for brand: AMEX,DC,DISC,MC,VISA,UNK
 	#Validate the user passed an acceptable values
 	if lbrand.strip() in gAllowedCardTypes:
 		if lbrand == "VISA": 	# Visa = 4
-			partial_card = partial_card.replace(partial_card[0],"4")
-			count_of_questionmarks = partial_card.count("?")
-			master_list = generateNumberList(count_of_questionmarks)
-			#get the first number in the list
+			partial_card = partial_card.replace(partial_card[0],"4",1)
+			unks = partial_card.count("?")
+			replnum_list = generateReplNumsList(unks)
+			valid_numbers = buildList(replnum_list, partial_card)
 			
-			for i in (len(master_list)):
-				for index_number in range(unknown_number_count):
-					replacement_number_string = master_list[index_number]
-					temp_card = partial_card
-					for char_index in range(count_of_questionmarks):
-						replacement_char = replacement_number_string[char_index]
-						index_of_card_char_to_replace = temp_card.find("?")
-						temp_card = temp_card.replace(temp_card[index_of_card_char_to_replace],replacement_char)
-						print("\nthere are "+str(temp_card.count("?"))+" left to replace.")
-					valid = False
-					valid = validateCard(temp_card)#check to see if the new full card number is a valid one
-					if valid == True:
-					valid_numbers.append(temp_card)#if it's valid, add it to the list
 		"""					
-		elif lbrand == "MC": # MasterCard = 51, 52, 53, 54, 55
-			#since MC has two known digits for all of its cards we only need to run the possible number set one
-			partial_card = partial_card.replace(partial_card[0,2],"50")
-			count = partial_card.count("?")
-			master_list.maxlen = count
-			master_list = generateNumberList(count)
-			
-			for i in range(5):
-				for i == 1:
-					partial_card[1] = "1"
-					print("go make the 51 series card numbers")
-				for i == 2:
-					partial_card[1] = "2"
-					print("go make the 52 series card numbers")
-				for i == 3:
-					partial_card[1] = "4"
-					print("go make the 53 series card numbers")	
-				for i == 4:
-					partial_card[1] = "4"
-					print("go make the 54 series card numbers")
-				for i == 5:
-					partial_card[1] = "5"
-					print("go make the 55 series card numbers")
-					
-			print("gotta make master card numbers")	
-		elif lbrand == "DISC":# Discover = 6011, 644, 65,
-			i = 0
-			for i in range (3):
-				for i == 1:
-					four = (partial_card.replace(partial_card[0,4] = "6011")					
-					count = four.count("?")
-					my_list4 = list()
-					my_list4.maxlen = count
-					my_list4 = generateNumberList(count)
-					#now go iterate through the possible numbers for each item in the list, replacing each ? with a digit
-				for i == 2:
-					three = (partial_card = partial_card.replace[0,3] = "644")
-					count = three.count("?")
-					my_list3 = list()
-					my_list3.maxlen = count
-					my_list3 = generateNumberList(count)
-					#now go iterate through the possible numbers for each item in the list, replacing each ? with a digit
-				for i == 3:
-					two = (partial_card = partial_card.replace[0,2] = "65")
-					count = two.count("?")
-					my_list2 = list()
-					my_list2.maxlen = count
-					my_list2 = generateNumberList(count)
-					#now go iterate through the possible numbers for each item in the list, replacing each ? with a digit
-
-			print("gotta make discover numbers")
-		elif lbrand == "AMEX":# Amex = 34, 37
-			#other areas of the code deal with 16 digits, let's, make sure there's only 15
-			partial_card = partial_card[:15]
-			#first let's just replace the the first two numbers
-			partial_card = partial_card.replace(partial_card[0,2],"34")
-			count = partial_card.count("?")
-			master_list.maxlen = count
-			master_list = generateNumberList(count)
-			#now control the loop to generate card numbers and only do runs using 51-55
-			print("gotta make amex numbers")			
-		elif lbrand == "DC":# Diner's Club = 36, 38
-			#first let's just replace the the first two numbers
-			partial_card = partial_card.replace(partial_card[0,2],"36")
-			count = partial_card.count("?")
-			master_list.maxlen = count
-			master_list = generateNumberList(count)
-			#now control the loop to generate card numbers and only do runs using 51-55
-			print("gotta make diners club numbers")		
-		elif lbrand == "UNK":
-			print("gotta make way too many numbers")
-			generateNumberList(partial_card.count("?"))
-		else:
-			print(lbrand)
+		insert code here to validate the other brands
 		"""
 	else: 
 		print("An invalid card type has been passed to the card generator.\n Allowed inputs are "+gAllowedCardTypes)
@@ -182,7 +134,7 @@ def cleanUpInput(userInput):
 		
 		#remove any spaces the user should not have entered
 		CardNumberUserInput = CardNumberUserInput.replace(" ","")
-		Warnings +=("\nYour entry contained spaces, they were removed.")
+		Warnings +=("\nIf your entry contained spaces, they were removed.")
 		
 		#truncate it down to the first 16 characters
 		if len(CardNumberUserInput) > 16:
@@ -190,8 +142,9 @@ def cleanUpInput(userInput):
 			CardNumberUserInput = (CardNumberUserInput[:16]) 
 			Warnings += ("\nYour input was truncated to: "+CardNumberUserInput)
 		else: 
+			Warnings += ("\nYour input did not have a minimum of 16 characters so ? were added to the end.")
 			for x in range(16-len(CardNumberUserInput)): #append question marks to the end if there are less than 16 digits provided
-				CardNumberUserInput.append("?")
+				CardNumberUserInput = CardNumberUserInput+"?"
 		#replace anything that isn't a number or a question mark with a question mark character
 		print("\n++++Your input is being validated for numbers and question marks.++++")
 		for i in range(len(CardNumberUserInput)): #this could be hard coded to say 0,17, but it would interfere if there is only 14-digit Diners Club card
@@ -203,7 +156,7 @@ def cleanUpInput(userInput):
 		if InvalidChars is not "": Warnings += "\nThe following invalid characters "+InvalidChars+ " were replaced with a ?"
 	except ValueError:
 		print("\nYou entered non string characters. Now go away.")
-	if Warnings != "": print("\n****The following WARNINGS were noted in processing your input\n"+Warnings+"\n****\n\nYour final card number after validation is "+CardNumberUserInput)
+	if Warnings != "": print("\n****The following WARNINGS were noted in processing your input\n"+Warnings+"\n****\n\nYour partial card number after validation is "+CardNumberUserInput)
 	else: print("\nYour input has passed all validation checks.")
 	return(CardNumberUserInput)
 	
@@ -212,6 +165,9 @@ def main():
 	PC = payment_card.PaymentCard("0", "UNK", "000000", "000000000000", exp_dt, "00000000", "John", "Q", "Doe", "")
 	UserInput = ""
 	partialPC = ""
+	semiFinalCardList = list()
+	validCardNumbers = list()
+
 	try:
 		UserInput = str(input("\nEnter the numbers you know. \n Replace unknown numbers with the ? symbol \n and do not use spaces or dashes.\n For example ????567890123456:\nThe numbers I know are:  "))
 		partialPC = cleanUpInput(UserInput)
@@ -226,64 +182,27 @@ def main():
 	else:
 		PC.brand = PC.brand_get(str(partialPC))
 		
-	gValidCardNumbers = generateCardNumbers(partialPC, PC.brand) 
+	semiFinalCardList = generateCardNumbers(partialPC, PC.brand) 
+	print("Before validation are "+str(len(semiFinalCardList))+" possible valid card numbers.")
 	
-	for i in range(len(gValidCardNumbers)):
-		with open("c:/temp/cardfile_"+str(datetime.now()), "w") as f:
-			f.write(gValidCardNumbers[i]+"\t")
-		f.close
+	for i in range(len(semiFinalCardList)):
+		if (validateCard(semiFinalCardList[i]) == True):
+			validCardNumbers.append(semiFinalCardList[i])
+	print("After validation there are "+str(len(validCardNumbers))+" possible VALID card numbers.")
+	
+	now = datetime.datetime.now() #generates a result of ""YYYY-MM-DD hh:mm:ss.######""
+	sNow = str(now)
+	sNow = sNow.replace(" ","_")
+	sNow = sNow.replace(":","")
+	sNow = sNow[:15]
+	file_name = "c:/temp/cardfile_"+sNow+".txt"	
+	
+	with open(file_name, "w") as f:
+		for i in range(len(validCardNumbers)):
+			f.write(validCardNumbers[i]+"\t")
+	f.close
 	print("Your results file has been placed in c:/temp/ the file name is cardfile_'today's date and time'")
 
 
 
 main()
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-"""
-	gUserCardInput = whatDoWeKnow() #we start with a gUserCardInput value of "unknown" and the function whatDoWeKnow should update this
-
-#Visa = 4
-#MasterCard = 51, 52, 53, 54, 55
-#Discover = 6011, 644, 65,
-#Amex = 34, 37
-#Diner's Club = 36, 38
-
-if gUserCardInput[0] == "?": 
-	gBrand = findIssuer(gUserCardInput) #we don't know the first number so let's see if the user can tell us who made the card
-	generateCardNumber(gUserCardInput, gBrand)
-elif gUserCardInput[0] == "4": generateCardNumber(gUserCardInput,"VISA") 
-elif gUserCardInput[0] == "5": generateCardNumber(gUserCardInput,"MC") 
-elif gUserCardInput[0] == "6": generateCardNumber(gUserCardInput,"DISC") 
-else:#so the first must be a 3, in which case we can determine if it is an AMEX or Diner's Club card provided we have the 2nd digit
-	if "AMEX" == isItAmexOrDC(gUserCardInput): generateCardNumber(gUserCardInput,"AMEX") 
-	elif "DC" == isItAmexOrDC(gUserCardInput): generateCardNumber(gUserCardInput,"DC") 
-	else: generateCardNumber(gUserCardInput,"UNK") 
-
-	
-	
-
-#The following code was written by JC-SoCal and can be found on git hub at 
-#https://github.com/JC-SoCal/CC_Playground/blob/master/ccluhnbrute.py
-#It has been modified to serve as the card builder for this program
-
-
-	
-#The following code was written by SecurityForUs and an be found on git hub at 
-#https://github.com/SecurityForUs/PythonUtils/blob/master/luhnalgo.py
-#It has been converted into a function here and the ValidCard variable has been added
-
-
-"""
